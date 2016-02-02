232e77bd3e15874616fedfdae8b0c94a540ebe19
UwenMaker <eucalyptus1230@gmail.com>
2016-02-02 23:57:56 +0800

    Initial Commit

---
+++ LeetCode-315/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LeetCode-315
+//
+//  Created by BruceChi on 2016/2/2.
+//  Copyright © 2016年 lazyBear. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
