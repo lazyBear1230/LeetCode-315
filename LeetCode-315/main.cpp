e49353f888bcaaa153684eefae2344fac88fc6f4
UwenMaker <eucalyptus1230@gmail.com>
2016-02-04 08:40:19 +0800

    # 315 normal solution, it will encounter run time exceed error

--- LeetCode-315/main.cpp
+++ LeetCode-315/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> countSmaller( vector<int>& nums )
+{
+    vector<int> countVec( nums.size(), 0 );
+    unsigned int idx, idx2;
+    int value, count;
+    for( idx = 0; idx < nums.size(); ++idx )
+    {
+        value = nums[idx];
+        count = 0;
+        for( idx2 = idx; idx2 < nums.size(); ++idx2 )
+        {
+            if( nums[idx2] >= value )
+                continue;
+            ++count;
+        }
+        countVec[idx] = count;
+    }
+    
+    return countVec;
+}
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    
+    
     return 0;
 }
